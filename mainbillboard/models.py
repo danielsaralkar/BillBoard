4409f2624faf616b91850b07cea33e846805e72c
Daniel Saralkar <danielsaralkar@gmail.com>
2017-08-19 11:52:08 +0300

    Before add post code

--- mainbillboard/models.py
+++ mainbillboard/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import datetime
+from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
+
 
 # Create your models here.
+@python_2_unicode_compatible
+class Messages(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=500)
+    author = models.CharField(max_length=100)
+    pub_date = models.DateTimeField('date published')
+
+    def _str_(self):
+        return self.title
--- mainbillboard/views.py
+++ mainbillboard/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from mainbillboard.models import Messages
+import json
 
 # Create your views here.
+def get_data(request):
+    myresult = Messages.object.all()
+    return render(request, 'index.html', {'comments': myresult})
+
+def add_post(request):
+    new_row = Messages()
+    return json.dumps({"STATUS":"SUCCESS"})
